9f88dbae8d7c2697eb9b8cc7f192a9aeebc01299
Marvin Dawson <marvintdawson@gmail.com>
2020-08-20 01:10:24 -0500

    added right navigation to header

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SiteDataService} from '../../../providers/site-data/site-data.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  siteName = this.siteData.siteName;
+  siteFooterName = this.siteData.siteFooterName;
+  date = new Date().getFullYear();
+  constructor(private siteData: SiteDataService) { }
 
   ngOnInit() {}
 
