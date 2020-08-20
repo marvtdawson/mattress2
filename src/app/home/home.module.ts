1a8f163f769cbfd5ff2e8e519344037b40d530c9
Marvin Dawson <marvintdawson@gmail.com>
2020-08-20 00:36:53 -0500

    added the styles for the scss files

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {LayoutModule} from '../layout/layout.module';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    LayoutModule
   ],
   declarations: [HomePage]
 })
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  pageTitle: any;
+  pageTitle = 'Mattress and Things';
   constructor() { }
 
   ngOnInit() {}
