068c93bf0e74f814010643a6541ec0670480c1b7
Marvin Dawson <marvintdawson@gmail.com>
2020-08-25 00:07:43 -0500

    created firebase config file and added to gitigonre

---
+++ src/app/app-firebase-config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: 'AIzaSyC3PYrXvBrK66fKwV0QBJt79mJPYXa2g1E',
+    authDomain: 'matandthingscd1973.firebaseapp.com',
+    databaseURL: 'https://matandthingscd1973.firebaseio.com',
+    projectId: 'matandthingscd1973',
+    storageBucket: 'matandthingscd1973.appspot.com',
+    messagingSenderId: '276364486417',
+    appId: '1:276364486417:web:80d9825cddd1d153df4eb0'
+};
