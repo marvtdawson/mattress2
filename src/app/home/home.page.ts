91851c81faae36948980e68e9261424a60583ed8
Marvin Dawson <marvintdawson@gmail.com>
2020-12-06 17:22:29 -0600

    refactor products dir to root, delete unused product dir

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,16 @@ export class HomePage {
   testImg = './assets/img/matts/amerisleep_mattress-iso_as3.jpg';
   socialMediaImg = './assets/img/site/social-icons/socialmediamarketing.jpg';
   thumbnailImg = false;
+  mattressImg = 'src/assets/img/matts/beautyrest_black_mariela.jpg';
+  bedroomImg = './assets/img/bed_rm/Tabasco_Storage_King_Bed_Styled.jpg';
+  livingroomImg = './assets/img/liv_rm/Compact-apartment-Moscow.jpg';
+  kitchenImg = './assets/img/kitch/kitchen_image_2.jpg';
+  diningroomImg = './assets/img/dining_rm/dining_tables-slides-0XQB-jumbo.jpg';
+  bathroomImg = './assets/img/bath_rm/bathroom_1.jpg';
+  lampImg = './assets/img/lamps/1-splash-mobile-Table-Lamps.jpg';
+  tableImg = './assets/img/tables/tyler-creek-counter-height-dining-room-table-0.jpg';
+  reclinerImg = './assets/img/recliners/lansing-tufted-leather-recliner-with-nailheads-o.jpg';
+  accessoriesImg = './assets/img/accessories/eb079052b1972b81297106d1c55c573e.jpg';
   public departments = [
     {
       thumbnailImg: true,
@@ -51,25 +61,25 @@ export class HomePage {
     {
       thumbnailImg: true,
       linkPath: '/lamps',
-      linkImage: './assets/img/lamps/',
+      linkImage: './assets/img/lamps/1-splash-mobile-Table-Lamps.jpg',
       linkText: 'Lamps'
     },
     {
       thumbnailImg: true,
       linkPath: '/tables',
-      linkImage: './assets/img/tables/',
+      linkImage: './assets/img/tables/tyler-creek-counter-height-dining-room-table-0.jpg',
       linkText: 'Tables'
     },
     {
       thumbnailImg: true,
       linkPath: '/recliners',
-      linkImage: './assets/img/recliners/',
+      linkImage: './assets/img/recliners/lansing-tufted-leather-recliner-with-nailheads-o.jpg',
       linkText: 'Recliners'
     },
     {
       thumbnailImg: true,
       linkPath: '/accessories',
-      linkImage: './assets/img/accessories',
+      linkImage: './assets/img/accessories/eb079052b1972b81297106d1c55c573e.jpg',
       linkText: 'Accessories'
     }
   ];
