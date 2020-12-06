0f0ecb7333d2811c759bd9ec1a96ecdc4c1f9263
Marvin Dawson <marvintdawson@gmail.com>
2020-12-06 07:50:30 -0600

    commented out custom css on footer to fix structure error

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -3,7 +3,7 @@ import {SiteDataService} from '../../../providers/site-data/site-data.service';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
+  templateUrl: './footer-backup.html',
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
